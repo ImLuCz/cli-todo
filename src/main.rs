e2a4feece0c2846a8d01abae4f1c75e4c216b577
ImLuCz <lucabrambilla3010@gmail.com>
2025-09-30 15:31:51 +0200

    feat: add remove argument with automatic index/value detection

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Cli {
 enum Commands {
     Add { task: String },
     List,
+    Remove { task: String },
 }
 
 #[derive(Serialize, Deserialize)]
@@ -24,10 +25,12 @@ fn main() {
     let cli = Cli::parse();
     let path = "todo.json";
 
+    // if file exists, the vector is loaded from the json
     let mut todo = if Path::new(path).exists() {
         let data = fs::read_to_string(path).unwrap();
         serde_json::from_str(&data).unwrap()
     } else {
+        // else it is created empty
         Todo { tasks: vec![] }
     };
 
@@ -35,10 +38,23 @@ fn main() {
         Commands::Add { task } => {
             todo.tasks.push(task);
             fs::write(path, serde_json::to_string_pretty(&todo).unwrap()).unwrap();
-            println!("Task added!");
+            println!("Task added");
+        }
+        Commands::Remove { task } => {
+            // if an index number was passed, remove the task at that index
+            if task.parse::<i32>().is_ok() {
+                // remove task by index
+                todo.tasks.remove(task.parse::<usize>().unwrap() - 1); // -1 because tasks are displayed starting from one
+            } else {
+                // remove task by value
+                todo.tasks.retain(|s| s != &task);
+            }
+            fs::write(path, serde_json::to_string_pretty(&todo).unwrap()).unwrap();
+            println!("Task removed");
         }
         Commands::List => {
             for (i, task) in todo.tasks.iter().enumerate() {
+                // prints tasks starting from 1 to be more user-friendly
                 println!("{}: {}", i + 1, task);
             }
         }
